396fdf398aac436b4a821585cf7630fc1375302e
Sarah Moran <s.l.a.moran@gmail.com>
2023-04-08 11:52:47 +0100

    Creation of search component,test, and CSS

---
+++ src/components/Search.js
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import "../styles/search.css";
+import getImages from "../requests/getImages";
+
+const Search = ({ setSearchResults }) => {
+  const [value, setValue] = useState();
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    setSearchResults(await getImages(value));
+  };
+
+  return (
+    <div className="search">
+      <form className="search-form" onSubmit={handleSubmit}>
+        <input
+          className="search-input"
+          type="text"
+          placeholder="enter search"
+          onChange={(e) => setValue(e.target.value)}
+        />
+        <button className="search-button" type="submit">
+          Go!
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Search;
