0d774c1a822a9f4cc31141ec26795a72dcdd7881
Sarah Moran <s.l.a.moran@gmail.com>
2023-04-08 11:44:15 +0100

    initial project commit

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../styles/app";
+
+const App = () => {
+  return (
+    <div className="app">
+      <p>Hello World!</p>
+    </div>
+  );
+};
+
+export default App;
