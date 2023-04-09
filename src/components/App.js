d12496bb6f73aa5f2da1112d6e455dcee4a88f42
Sarah Moran <s.l.a.moran@gmail.com>
2023-04-09 16:09:06 +0100

    Creation of SearchResults component, test, and styling

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,21 @@
-import React from "react";
-import "../styles/app";
+import React, { useState } from "react";
+import "../styles/app.css";
+import Search from "./Search";
+import SearchResults from "./SearchResults";
 
 const App = () => {
+  const [searchResults, setSearchResults] = useState([]);
+
+  //const searchResult = searchResults.map((e) => console.log(e));
   return (
     <div className="app">
-      <p>Hello World!</p>
+      <img
+        className="nasa-logo"
+        src="https://cdn.cnn.com/cnnnext/dam/assets/200424060716-nasa-worm-logo.jpg"
+        alt="nasaLogo"
+      />
+      <Search setSearchResults={setSearchResults} />
+      <SearchResults results={searchResults} />
     </div>
   );
 };
---
+++ src/components/SearchResults.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "../styles/search-results.css";
+
+const SearchResults = ({ results }) => {
+  if (!results.length) {
+    return <p className="no-results">No Results</p>;
+  } else {
+    return (
+      <>
+        <div className="image-gallery">
+          {results.map((image) => (
+            <img className="card-image" src={image} alt="requested-result" />
+          ))}
+        </div>
+      </>
+    );
+  }
+};
+
+export default SearchResults;
